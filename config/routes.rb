b91ac675d6379d93a31a326eb56982e671db181d
deeptika <deeptika24@gmail.com>
2020-06-19 13:54:34 +0530

    Added user login feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get "/", to: "sessions#new"
+  get "/signin", to: "sessions#new", as: :new_sessions
+  post "/signin", to: "sessions#create", as: :sessions
+  delete "/signout", to: "sessions#destroy", as: :destroy_session
   resources :users
   resources :orders
   resources :order_items
   resources :menus
   resources :menu_items
   resources :carts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
