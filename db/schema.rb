1a4af78cedc13b09fb9ec1f5caafa74f3ffcc405
deeptika <deeptika24@gmail.com>
2020-06-20 21:32:17 +0530

    Formatted all views

--- db/schema.rb
+++ db/schema.rb
@@ -59,13 +59,6 @@ ActiveRecord::Schema.define(version: 2020_06_19_065659) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "todos", force: :cascade do |t|
-    t.text "todo_text"
-    t.date "due_date"
-    t.boolean "completed"
-    t.bigint "user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
